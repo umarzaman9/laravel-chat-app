7e468da00021a416b43e9fc9b92db61ab07ae0e2
Umer Zaman <umer.zaman@techswivel.com>
2026-06-03 13:03:41 +0500

    added avatar upload feature

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,14 +32,29 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'avatar' => [
+                'nullable',
+                'image',
+                'max:5120',
+                'mimes:jpeg,png,jpg'
+            ]
         ]);
+
+        if ($request->hasFile('avatar')) {
+            $file = $request->file('avatar');
+
+            $filename = time() . '.' . $file->extension();
+            $file->move(public_path('images'), $filename);
+            $avatar = 'images/' . $filename;
+        }
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'avatar' => $avatar
         ]);
 
         event(new Registered($user));
